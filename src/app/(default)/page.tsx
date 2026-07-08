541782d11dfea2d371f01b3a527697e1eb66abc3
munnakumar046 <munnakumar15006@gmail.com>
2026-07-08 11:19:28 +0530

    mongodb removed

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -1,13 +1,6 @@
 import { Button } from "@/components/ui/button";
-import dbConnect from "@/lib/mongodb";
-
-import Posts from "@/models/Posts";
 
 export default async function Home() {
-  await dbConnect();
-
-  const posts = await Posts.find();
-  console.log(posts);
   return (
     <div>
       <h1>Home Page</h1>
